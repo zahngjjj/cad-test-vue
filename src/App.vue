fc94a3382367f86af7fdc6ff706678f39be8124e
zhangjinjian <1016417093@qq.com>
2025-08-07 14:22:09 +0800

    feat: 优化

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,8 @@
     <!-- <CAD3DViewer/> -->
     <!-- <ProcessFlowDiagram/> -->
     <!-- <D3ProcessFlow></D3ProcessFlow> -->
-    <FactorySimulation></FactorySimulation>
+    <!-- <FactorySimulation></FactorySimulation> -->
+    <GridFactoryDemo></GridFactoryDemo>
   </div>
 </template>
 
@@ -19,7 +20,8 @@
 // import CAD3DViewer from './components/CAD3DViewer.vue'
 // import ProcessFlowDiagram from './components/ProcessFlowDiagram.vue'
 // import D3ProcessFlow from './components/D3ProcessFlow.vue'
-import FactorySimulation from './components/FactorySimulation.vue'
+// import FactorySimulation from './components/FactorySimulation.vue'
+import GridFactoryDemo from './components/GridFactoryDemo.vue'
 
 </script>
 
